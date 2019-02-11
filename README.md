d51ab3cbb9086026f222aca2c03e89dd38d71bf3
Frederic Lemay <461102+flemay@users.noreply.github.com>
2019-02-12 09:39:29 +1100

    Script to clean function policy (#6)
    * clean function policy
    
    * fix to avoid TooManyRequestsException: Rate exceeded error
    
    * fix waiting loop
    
    * add doc

--- README.md
+++ README.md
@@ -23,3 +23,37 @@ $ make deploy
 # Remove the lambda stack
 $ make remove
 ```
+
+## Scrips
+
+### clean_policy.js
+
+This script removes all permissions of the sumologic lambda and adds a generic one that looks like the following:
+
+```json
+{
+  "Version": "2012-10-17",
+  "Id": "default",
+  "Statement": [
+    {
+      "Sid": "GenericInvokePermissionForCloudWatchLogs-1549531551541",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "logs.awsregion.amazonaws.com"
+      },
+      "Action": "lambda:InvokeFunction",
+      "Resource": "your:lambda:function:arn",
+      "Condition": {
+        "StringEquals": {
+          "AWS:SourceAccount": "awsaccount"
+        },
+        "ArnLike": {
+          "AWS:SourceArn": "arn:aws:logs:awsregion:awsaccount:*"
+        }
+      }
+    }
+  ]
+}
+```
+
+Make sure your environment variables are set properly and run `$ make deps cleanPolicy`.
---
+++ scripts/clean_policy.js
@@ -0,0 +1,67 @@
+// This script is to clean up Lambda Policy by adding a generic lambda permission
+// that allows CloudWatch Logs to invoke the lambda FUNCTION_NAME and removing all
+// other permissions.
+// This was created because the function's policy size exceeded the limit due to
+// serverless-log-forward plugin adding the same permission whenever a new stack
+// was creted. https://github.com/amplify-education/serverless-log-forwarding/pull/22
+const AWS = require('aws-sdk')
+
+const getEnvironmentVariableValue = (name) => {
+  if (!process.env[name]) {
+    throw new Error(`Environment variable "${name}" is required and must not be empty`)
+  }
+  return process.env[name]
+}
+
+// addGenericPermissionForCloudWatchLogs creates a generic permission with a
+// unique statementID that allows CloudWatch Logs of a specific aws region in
+// a specific account to invoke lambda functionName
+const addGenericPermissionForCloudWatchLogs = (awsAccountId, awsRegion, lambda,
+  functionName, statementId) => {
+  const params = {
+    StatementId: statementId,
+    Action: 'lambda:InvokeFunction',
+    FunctionName: functionName,
+    Principal: 'logs.ap-southeast-2.amazonaws.com',
+    SourceArn: `arn:aws:logs:${awsRegion}:${awsAccountId}:*`,
+    SourceAccount: awsAccountId,
+  }
+  return lambda.addPermission(params).promise()
+}
+
+const wait = milleseconds => new Promise(resolve => setTimeout(resolve, milleseconds))
+
+// removePermissions removes all permission of lambda functionName and leaves
+// permission with statementId exceptGenericPermissionSid untouched.
+const removePermissions = async (lambda, functionName, exceptGenericPermissionSid) => {
+  const data = await lambda.getPolicy({ FunctionName: functionName }).promise()
+  const policy = JSON.parse(data.Policy)
+
+  for (let i = 0; i < policy.Statement.length; i += 1) {
+    if (policy.Statement[i].Sid !== exceptGenericPermissionSid) {
+      console.log(`removing permission ${policy.Statement[i].Sid}`)
+
+      /* eslint-disable no-await-in-loop */
+      await lambda.removePermission({ FunctionName: functionName,
+        StatementId: policy.Statement[i].Sid }).promise()
+      // delay to avoid "TooManyRequestsException: Rate exceeded" error
+      await wait(1500)
+      /* eslint-enable no-await-in-loop */
+    }
+  }
+}
+
+(async () => {
+  const env = getEnvironmentVariableValue('ENV')
+  const serviceName = getEnvironmentVariableValue('SERVICE_NAME')
+  const awsAccountId = getEnvironmentVariableValue('AWS_ACCOUNT_ID')
+  const awsRegion = getEnvironmentVariableValue('AWS_REGION')
+
+  const lambda = new AWS.Lambda()
+  const statementId = `GenericInvokePermissionForCloudWatchLogs-${Date.now()}`
+  const functionName = `${serviceName}-${env}-publish`
+
+  await addGenericPermissionForCloudWatchLogs(awsAccountId, awsRegion, lambda,
+    functionName, statementId)
+  await removePermissions(lambda, functionName, statementId)
+})()
